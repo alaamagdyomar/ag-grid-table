ee6cec126e0863c9f444a5b25348053b0f6782dd
alaamagdy <alaamagdy@paymob.com>
2022-12-30 20:10:36 +0200

    duplicate row is enabled

--- src/components/icons.js
+++ src/components/icons.js
@@ -5,39 +5,9 @@ import { useRef ,useCallback} from 'react';
 import './comp.css'
 
 const RenderIcons = (props)=>{
-    function createDataItem(
-        id,
-        name,
-        last_name,
-        status,
-      ) {
-        return {
-          id: id,
-          name: name,
-          last_name:last_name,
-          status:status
-        };
-      }
-
     const onBtDuplicate = useCallback(() => {
-        var api = props.gridRef.current.api;
-        // get the first child of the
-        var selectedRows = api.getSelectedRows();
-        console.log('selected rows =',selectedRows)
-        if (!selectedRows || selectedRows.length === 0) {
-          console.log('No rows selected!');
-          return;
-        }
-        var newItems = [];
-        selectedRows.forEach(function (selectedRow) {
-          var newItem = createDataItem(
-            selectedRow.id,
-            selectedRow.name,
-            selectedRow.last_name,
-            selectedRow.status,
-          );
-          newItems.push(newItem);
-        });
+        var selectedRows = props.gridRef.current.api.getSelectedRows();
+        props.gridRef.current.api.applyTransaction({add:selectedRows})
       }, []);
 
       const removeSelected = useCallback(()=>{
